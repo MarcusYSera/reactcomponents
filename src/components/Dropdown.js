15a8e824484f6ce7ed60c7be3a9e1fa1586d360c
Marcus Sera <m.yoshiosera@gmail.com>
2020-07-07 02:41:06 -0700

    almost done

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,21 +3,59 @@ import React, { Component } from 'react';
 import '../global.css';
 
 class App extends Component {
-  state = {};
+  state = { open: false };
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.clickedOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.clickedOutside);
+  }
+
+  setWrapperRef = (e) => {
+    // console.log('burger menu');
+    this.wrapperRef = e;
+  };
+
+  clickedOutside = (e) => {
+    if (this.wrapperRef && !this.wrapperRef.contains(e.target)) {
+      this.setState({
+        open: false,
+      });
+    }
+  };
 
   openDropDown = () => {
-    console.log('clicked');
+    const { open } = this.state;
+    let answer = false;
+    if (open === false) {
+      answer = open === false ? true : answer;
+    }
+    if (open === true) {
+      answer = open === true ? answer : true;
+    }
+    this.setState({
+      open: answer,
+    });
   };
 
   render() {
+    const { open } = this.state;
     return (
       <div className="dropdownwrapper">
         <div className="dropdownheader">
-          <i className="bars icon" onClick={() => this.openDropDown()} />
+          <i
+            className="bars icon"
+            onClick={this.openDropDown}
+            ref={this.setWrapperRef}
+          />
         </div>
-        <ul className="dropdownlistwrapper">
-          <li className="dropdownitem">contents</li>
-        </ul>
+        {open === true ? (
+          <ul ref={this.setWrapperRef} className="dropdownlistwrapper">
+            <li className="dropdownitem">contents</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
