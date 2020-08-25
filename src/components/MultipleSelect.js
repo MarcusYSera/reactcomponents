87bf186500fbc55b284b4feba76603f128fe971d
Marcus Sera <m.yoshiosera@gmail.com>
2020-08-24 23:03:45 -0700

    working on click events for submit with onblur

--- src/components/MultipleSelect.js
+++ src/components/MultipleSelect.js
@@ -1,56 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-// const clickOpen = () => {
-//   console.log('clicked');
-//   return null;
-// };
+class MultipleSelect extends Component {
+  state = { value: '' };
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  console.log(event);
-};
+  handleBlur = (event) => {
+    console.log('onBlur');
+    this.setState({ value: event.target.value });
+  };
 
-const multipleSelect = () => {
-  return (
-    // <button
-    //   type="button"
-    //   className="ui fluid dropdown selection multiple"
-    //   onClick={clickOpen}
-    //   onKeyDown={clickOpen}
-    // >
-    <form onSubmit={handleSubmit}>
-      <select name="skills" multiple>
-        <option value="">Skills</option>
-        <option value="angular">Angular</option>
-        <option value="css">CSS</option>
-        <option value="design">Graphic Design</option>
-        <option value="ember">Ember</option>
-        <option value="html">HTML</option>
-        <option value="ia">Information Architecture</option>
-        <option value="javascript">Javascript</option>
-        <option value="mech">Mechanical Engineering</option>
-        <option value="meteor">Meteor</option>
-        <option value="node">NodeJS</option>
-        <option value="plumbing">Plumbing</option>
-        <option value="python">Python</option>
-        <option value="rails">Rails</option>
-        <option value="react">React</option>
-        <option value="repair">Kitchen Repair</option>
-        <option value="ruby">Ruby</option>
-        <option value="ui">UI Design</option>
-        <option value="ux">User Experience</option>
-      </select>
-      <input type="submit" value="Submit" />
-    </form>
-    // <i className="dropdown icon" />
-    // <div className="default text">Skills</div>
-    // <div className="menu transition hidden">
-    //   <div className="item" data-value="angular">
-    //     angular
-    //   </div>
-    // </div>
-    // </button>
-  );
-};
+  handleChange = () => {
+    console.log('onChange');
+  };
 
-export default multipleSelect;
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { value } = this.state;
+    console.log(value);
+  };
+
+  render() {
+    const { value } = this.state;
+    return (
+      // <button
+      //   type="button"
+      //   className="ui fluid dropdown selection multiple"
+      //   onClick={clickOpen}
+      //   onKeyDown={clickOpen}
+      // >
+      <form onSubmit={this.handleSubmit}>
+        <select
+          name="skills"
+          onBlur={this.handleBlur}
+          // onChange={this.handleChange}
+          value={value}
+        >
+          <option value="">Skills</option>
+          <option value="angular">Angular</option>
+          <option value="css">CSS</option>
+          <option value="design">Graphic Design</option>
+          <option value="ember">Ember</option>
+          <option value="html">HTML</option>
+          <option value="ia">Information Architecture</option>
+          <option value="javascript">Javascript</option>
+          <option value="mech">Mechanical Engineering</option>
+          <option value="meteor">Meteor</option>
+          <option value="node">NodeJS</option>
+          <option value="plumbing">Plumbing</option>
+          <option value="python">Python</option>
+          <option value="rails">Rails</option>
+          <option value="react">React</option>
+          <option value="repair">Kitchen Repair</option>
+          <option value="ruby">Ruby</option>
+          <option value="ui">UI Design</option>
+          <option value="ux">User Experience</option>
+        </select>
+        <input type="submit" value="Submit" />
+      </form>
+      // <i className="dropdown icon" />
+      // <div className="default text">Skills</div>
+      // <div className="menu transition hidden">
+      //   <div className="item" data-value="angular">
+      //     angular
+      //   </div>
+      // </div>
+      // </button>
+    );
+  }
+}
+
+export default MultipleSelect;
